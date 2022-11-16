4553450535cde9e0842a2848aafe776467f8c74f
Alvin Daeli <alvindaeli748@gmail.com>
2022-11-16 11:10:51 +0700

    progress followers

--- app/Http/Controllers/ActionController.php
+++ app/Http/Controllers/ActionController.php
@@ -4,10 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Helpers\Helpers;
+use App\Models\User;
+use App\Models\Follower;
+use App\Models\Following;
 use Exception;
 use Illuminate\Http\Client\RequestException;
 
 class ActionController extends Controller
 {
-    //
+    public function followUser(Request $request)
+    {
+        try {
+            $request->validate([
+                'user_id'=> 'required',
+                'target_user'=> 'required'
+            ]);
+
+            $following = Following::create([
+                'user_id' => $request->user_id,
+                'user_followed_id' => $request->target_user,
+            ]);
+
+            $follower = Follower::create([
+                'user_id' => $following->user_followed_id,
+                'user_follower_id' => $following->user_id,
+            ]);
+
+            $data = User::where('users.id','=',$following->user_id)->join('followings', 'users.id','=','followings.user_id');
+
+            return print_r($data);
+            if($data){
+                return Helpers::getData(200, 'Success', $data);
+            } else {
+                return Helpers::getData(400, 'Failed');
+            }
+
+        } catch (Exception $err) {
+            return $err;
+        }
+    }
+
+    public function unfollowUser(Request $request)
+    {
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,9 @@ Route::post('posts/commentPost', [PostController::class, 'commentPost']);
 
 Route::get('search/{param}', [SearchController::class, 'search']);
 
+Route::post('followUser', [ActionController::class, 'followUser']);
+Route::post('unfollowUser', [ActionController::class, 'unfollowUser']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
